0d5f085a269fd213aeb9488866379a8eba8716ed
Mark Wolfe <mark@wolfe.id.au>
2024-11-23 14:33:19 +1100

    fix: adjust the options for save and cli

--- internal/commands/save.go
+++ internal/commands/save.go
@@ -10,10 +10,9 @@ import (
 )
 
 type SaveCmd struct {
-	Key         string `help:"Key for a cache entry." env:"INPUT_KEY"`
-	Path        string `help:"Path list for a cache entry." env:"INPUT_PATH"`
-	RestoreKeys string `help:"Restore keys list for a cache entry." env:"INPUT_RESTORE_KEYS"`
-	Endpoint    string `help:"Endpoint for a cache entry." env:"INPUT_ENDPOINT"`
+	Key      string `help:"Key for a cache entry." env:"INPUT_KEY"`
+	Path     string `help:"Path list for a cache entry." env:"INPUT_PATH"`
+	Endpoint string `help:"Endpoint for a cache entry." env:"INPUT_ENDPOINT"`
 }
 
 func (cmd *SaveCmd) Run(ctx context.Context, globals *Globals) error {
@@ -22,7 +21,6 @@ func (cmd *SaveCmd) Run(ctx context.Context, globals *Globals) error {
 	fmt.Printf("::notice endpoint=%s\n", cmd.Endpoint)
 	fmt.Printf("::notice key=%s\n", cmd.Key)
 	fmt.Printf("::notice path=%s\n", cmd.Path)
-	fmt.Printf("::notice restore-keys=%s\n", cmd.RestoreKeys)
 
 	if cmd.Endpoint == "http://localhost:8080" {
 		return nil
